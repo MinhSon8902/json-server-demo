bcfcd995227bbf9ffe7a416ae7f918bf7db19c3f
Minh Son <minhson892002@gmail.com>
2022-12-19 23:47:02 +0700

    random data categories

--- generate-data.js
+++ generate-data.js
@@ -1,12 +1,42 @@
-var faker = require("faker");
+const faker = require("faker");
+const fs = require("fs");
 
 // set locale to use vietnamese
 faker.locale = "vi";
 
-console.log(faker.commerce.department());
-console.log(faker.commerce.productName());
-console.log(faker.commerce.productDescription());
-console.log(faker.image.cats());
-console.log(faker.commerce.product());
-console.log(faker.random.imageUrl());
-console.log(faker.random.findName());
+// console.log(faker.datatype.uuid());
+// console.log(faker.commerce.product());
+// console.log(faker.company.companyName());
+// console.log(faker.image.cats());
+
+const randomCategoryList = (n) => {
+  if (n <= 0) return [];
+  const categoryList = [];
+
+  Array.from(new Array(n)).forEach(() => {
+    const category = {
+      id: faker.random.uuid(),
+      name: faker.commerce.department(),
+      createAt: Date.now(),
+      updateAt: Date.now(),
+    };
+    categoryList.push(category);
+  });
+
+  return categoryList;
+};
+
+(() => {
+  const categoryList = randomCategoryList(4);
+  const db = {
+    categories: categoryList,
+    products: [],
+    profile: {
+      name: "MS",
+    },
+  };
+
+  fs.writeFile("db.json", JSON.stringify(db), () => {
+    console.log("Generate data successfully !");
+  });
+})();
