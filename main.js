6dbbace337ebef939d6f3677fad0cadab8f78eb8
Minh Son <minhson892002@gmail.com>
2022-12-20 17:18:24 +0700

    update port

--- main.js
+++ main.js
@@ -51,8 +51,10 @@ router.render = (req, res) => {
   // Otherwise, keep default behavior
   res.jsonp(res.locals.data);
 };
-// Use default router
+
 server.use('/api', router);
-server.listen(3000, () => {
+// Use default router
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
   console.log('JSON Server is running');
 });
